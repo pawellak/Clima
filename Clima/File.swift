9000398f7616075029b257bf2ce08595d3c7960c
Paweł Łąk <pawellak@gmail.com>
2025-05-23 15:39:18 +0200

    view controller + extensions

---
+++ Clima/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  Clima
+//
+//  Created by Paweł Łąk on 21/05/2025.
+//  Copyright © 2025 App Brewery. All rights reserved.
+//
+
+import Foundation
---
+++ Clima/WeatherModel.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherModel.swift
+//  Clima
+//
+//  Created by Paweł Łąk on 21/05/2025.
+//  Copyright © 2025 App Brewery. All rights reserved.
+//
+
+import Foundation
